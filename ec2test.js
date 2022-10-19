7bce3b4ae395ef4d297bf20f9818e53e273bec72
cvogel3434 <cvogel.a.m@gmail.com>
2022-10-19 18:26:40 -0500

    kkd

--- ec2test.js
+++ ec2test.js
@@ -10,7 +10,6 @@ var result = {};
 var testgitfun=()=>{
   var teststore = require('./store/storemaps/storemap.json').store.apps.VMT.wos;
 
-
   var testcon = new NEDBconnect({filename:path.join(__dirname,'store/apps/VMT/masterwos.db')},teststore.ensure);
   testcon.docs.loadDatabase();
 
@@ -20,11 +19,11 @@ var testgitfun=()=>{
     res=>{
       console.log(res);
       addvar.root = stdout;
-      exec(`git add .`,{cwd:'store'},(err,stdout,stderr)=>{
+      exec(`git add .`,(err,stdout,stderr)=>{
         addvar.gitadd={err:err,stdout:stdout,stderr:stderr};
-        exec(`git commit -m "database add"`,{cwd:'store'},(err,stdout,stderr)=>{
+        exec(`git commit -m "database add"`,(err,stdout,stderr)=>{
           addvar.gitcomitt={err:err,stdout:stdout,stderr:stderr};
-          exec(`git push origin main`,{cwd:'store'},(err,stdout,stderr)=>{
+          exec(`git push origin main`,(err,stdout,stderr)=>{
             addvar.gitpush={err:err,stdout:stdout,stderr:stderr};
           });
         });
