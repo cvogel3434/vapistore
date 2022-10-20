d2848dfa3b8d6c29335cd296992717c35281aed8
cvogel3434 <cvogel.a.m@gmail.com>
2022-10-19 19:08:14 -0500

    adj;fla

--- ec2test.js
+++ ec2test.js
@@ -6,13 +6,20 @@ var result = {};
 
 var testgitfun=()=>{
   var teststore = require('./store/storemaps/storemap.json').store.apps.VMT.wos;
-
+  var testvar = {}
   var testcon = new NEDBconnect({filename:path.join(__dirname,'store/apps/VMT/masterwos.db')},teststore.ensure);
   testcon.docs.loadDatabase();
 
   testvar.id=81;
   testcon.INSERTdb(testvar).then(
     res=>{
+      exec(`git add .`,(err,stdout,stderr)=>{
+        exec(`git commit -m "database add"`,(err,stdout,stderr)=>{
+          exec(`git push origin main`,(err,stdout,stderr)=>{
+            
+          });
+        });
+      });
     }
   );
 }
